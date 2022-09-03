b52e78b4249447d4d5dd31c06493cdbbfa5e5692
higorsilveira <higorsilveira.hs@gmail.com>
2022-09-03 16:42:46 -0300

    Colisão entre inimigo e jogador

--- 2d/Scripts/Mushroom.gd
+++ 2d/Scripts/Mushroom.gd
@@ -1,21 +1,23 @@
 extends KinematicBody2D
 
-export var speed = 64
-export var health = 1
+export var speed = 32
+export var health = 2
 var velocity = Vector2.ZERO
 var move_direction = -1
+var gravity = 1200
+var hittted = false
 
 func _physics_process(delta):
 	velocity.x = speed * move_direction
-	
-	velocity = move_and_slide(velocity)
+	velocity.y += gravity * delta
 	
 	if move_direction == 1:
 		$texture.flip_h = true
 	else: $texture.flip_h = false
 	
-	if $ray_wall.is_colliding():
-		$anim.play("Idle")
+	_set_animation()
+	
+	velocity = move_and_slide(velocity)
 
 func _on_anim_animation_finished(anim_name):
 	if anim_name == "Idle":
@@ -23,3 +25,27 @@ func _on_anim_animation_finished(anim_name):
 		$ray_wall.scale.x *= -1
 		move_direction *= -1
 		$anim.play("Run")
+
+func _set_animation():
+	var anim = "Run"
+	
+	if $ray_wall.is_colliding():
+		anim = "Idle"
+	elif velocity.x != 0:
+		anim = "Run"
+	
+	if hittted == true:
+		anim = "Hit"
+	
+	if $anim.assigned_animation != anim:
+		$anim.play(anim)
+
+func _on_hitBox_body_entered(body):
+	hittted = true
+	health -= 1
+	body.velocity.y -= 300
+	yield(get_tree().create_timer(0.2), "timeout")
+	hittted = false
+	if health < 1:
+		get_node("hitBox/collision").set_deferred("disabled", true)
+		queue_free()
--- 2d/Scripts/Player.gd
+++ 2d/Scripts/Player.gd
@@ -5,6 +5,10 @@ var move_speed = 480
 var gravity = 1200
 var jump_force = -720
 var is_grounded
+var health = 3
+var hurted = false
+var knockback_dir = 1
+var knockback_force = 600
 onready var raycasts = $raycasts
 
 func _ready():
@@ -23,7 +27,8 @@ func _get_input():
 	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2)
 
 	if move_direction != 0:
-		$texture.scale.x = move_direction	
+		$texture.scale.x = move_direction
+		knockback_dir = move_direction
 
 func _input(event):
 	if event.is_action_pressed("jump") && is_grounded:
@@ -44,6 +49,26 @@ func _set_animation():
 		else: anim = "Fall"
 	elif velocity.x != 0:
 		anim = "Run"
+		
+	if hurted:
+		anim = "Hit"
 	
 	if $anim.assigned_animation != anim:
 		$anim.play(anim)
+
+func knockback():
+	velocity.x = -knockback_dir * knockback_force
+	velocity.y = -knockback_force / 4
+	velocity = move_and_slide(velocity)
+
+func _on_hurtBox_body_entered(body):
+	health -= 1
+	hurted = true
+	knockback()
+	get_node("hurtBox/collision").set_deferred("disabled", true)
+	yield(get_tree().create_timer(0.5),"timeout")
+	get_node("hurtBox/collision").set_deferred("disabled", false)
+	hurted = false
+	if health < 1:
+		queue_free()
+		get_tree().reload_current_scene()
