9f2392efd09c4154462d7c0bbeec571eb3ad6145
higorsilveira <higorsilveira.hs@gmail.com>
2022-09-04 10:15:26 -0300

    HUD + singleton

--- 2d/Scripts/Items.gd
+++ 2d/Scripts/Items.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
+export var fruits = 1
+
 func _on_items_body_entered(body):
+	Global.fruits += fruits
+	print(Global.fruits)
 	$anim.play("Collected")
 
 func _on_anim_animation_finished(anim_name):
